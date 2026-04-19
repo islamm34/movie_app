1e1c88bdd80e15f92f7c05dc59e92efdb3d70edc
Islam Mohamed <https://github.com/islamm34>
2026-04-20 01:16:39 +0200

    add main.dart

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'core/utilities/app_routs.dart';
+
+void main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    _removeSplash();
+  }
+
+  void _removeSplash() async {
+    await Future.delayed(const Duration(seconds: 2));
+    FlutterNativeSplash.remove();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Movie App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      routes: AppRoutes.routes,
+      initialRoute: _getInitialRoute(),
+    );
+  }
+
+  String _getInitialRoute() {
+    final User? currentUser = FirebaseAuth.instance.currentUser;
+
+    if (currentUser != null) {
+      print('User is logged in: ${currentUser.email}');
+      return AppRoutes.home;
+    } else {
+      print('User is not logged in, redirecting to login');
+      return AppRoutes.login;
+    }
+  }
+}
